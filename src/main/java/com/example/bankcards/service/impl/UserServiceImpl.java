f3fcff1ef2b8deb17a9cf64d81a84a84d7713ce5
Adis-cmd <piv4484@gmail.com>
2026-03-04 19:25:15 +0500

    feat(service): implement UserService for user operations
    Add UserService to handle business logic related to User entity.

---
+++ src/main/java/com/example/bankcards/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.service.impl;
+
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+}
