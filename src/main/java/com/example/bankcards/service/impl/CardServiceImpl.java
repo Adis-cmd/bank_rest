9fb542773a9d51837da6958d4704dd82518b769d
Adis-cmd <piv4484@gmail.com>
2026-03-04 19:35:38 +0500

    feat(card): implement repository and service for Card entity
    Add CardRepository for DB operations and CardService to handle business logic.

---
+++ src/main/java/com/example/bankcards/service/impl/CardServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.bankcards.service.impl;
+
+import com.example.bankcards.service.CardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CardServiceImpl implements CardService {
+}
