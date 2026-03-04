86d609107a55bde5f4e75592d33f83bb03f7b9de
Adis-cmd <piv4484@gmail.com>
2026-03-04 19:27:25 +0500

    feat(exception): implement AuthorityNotFoundException
    Add custom runtime exception to signal missing Authority entities.

---
+++ src/main/java/com/example/bankcards/exception/AuthorityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.bankcards.exception;
+
+public class AuthorityNotFoundException extends RuntimeException  {
+    public AuthorityNotFoundException(String message) {
+        super(message);
+    }
+}
