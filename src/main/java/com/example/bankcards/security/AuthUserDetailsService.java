35052b5676a11414d5853151f715ae16146129cd
Adis-cmd <piv4484@gmail.com>
2026-03-04 16:52:51 +0500

    feat(security): implement AuthUserDetailsService Add loadUserByUsername to fetch user by email and map to Spring Security UserDetails.

---
+++ src/main/java/com/example/bankcards/security/AuthUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.bankcards.security;
+
+import com.example.bankcards.entity.User;
+import com.example.bankcards.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                Collections.singleton(new SimpleGrantedAuthority(user.getAuthority().getName()))
+        );
+    }
+}
