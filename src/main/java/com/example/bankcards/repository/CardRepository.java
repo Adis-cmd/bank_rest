682422498a0ed0e7e47714a3986afc12d2b966ff
Adis-cmd <piv4484@gmail.com>
2026-03-07 19:36:38 +0500

    feat(transactions): add missing repository queries for card and transactions

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -1,9 +1,22 @@
 package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.Card;
+import com.example.bankcards.enums.CardStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+    Boolean existsByCardNumber(String cardNumber);
+
+
+    @Query("select c from Card c where c.owner.id = :userId")
+    Page<Card> findAllByOwnerId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("select c from Card c where c.owner.id = :userId and c.status = :status")
+    Page<Card> findByOwnerIdAndStatus(@Param("userId") Long userId, @Param("status") CardStatus status, Pageable pageable);
 }
--- src/main/java/com/example/bankcards/repository/TransactionRepository.java
+++ src/main/java/com/example/bankcards/repository/TransactionRepository.java
@@ -2,8 +2,20 @@ package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("delete from Transaction t WHERE t.fromCard.id = :fromCardId or t.toCard.id = :toCardId")
+    void deleteByFromCardIdOrToCardId(
+            @Param("fromCardId") Long fromCardId,
+            @Param("toCardId") Long toCardId);
+
 }
